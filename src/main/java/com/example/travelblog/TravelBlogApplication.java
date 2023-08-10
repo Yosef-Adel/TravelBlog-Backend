a483e0e946929ae3e03166e5da127c86145e121d
Yosef <yosef.saaid02@eng-st.cu.edu.eg>
2023-08-10 10:23:54 +0300

    adding Security and JWT

--- src/main/java/com/example/travelblog/TravelBlogApplication.java
+++ src/main/java/com/example/travelblog/TravelBlogApplication.java
@@ -1,13 +1,35 @@
 package com.example.travelblog;
 
+import com.example.travelblog.models.Role;
+import com.example.travelblog.models.User;
+import com.example.travelblog.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TravelBlogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TravelBlogApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner commandLineRunner(UserService userService) {
+        return args -> {
+
+            userService.addRole(new Role("ROLE_USER"));
+
+            User user = new User("yosef", "yosef@gmail.com", "fun123");
+
+            userService.addUser(user);
+
+            userService.addRole(new Role("ROLE_ADMIN"));
+
+            userService.attachRoleToUser("yosef@gmail.com", "ROLE_ADMIN");
+
+
+        };
     }
 
 }
---
+++ src/main/java/com/example/travelblog/config/SecurityConfig.java
@@ -0,0 +1,79 @@
+package com.example.travelblog.config;
+
+/*
+ * @created 10/08/2023 - 9:14 AM
+ * @project TravelBlog
+ * @author Yosef Adel Mahmoud Saaid
+ */
+
+import com.example.travelblog.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(UserService userService){
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(userService);
+        auth.setPasswordEncoder(passwordEncoder());
+        return auth;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers( "/api/v1/**").hasRole("ADMIN")
+                        .requestMatchers( "/api/v1/post/**").hasRole("USER")
+                        .requestMatchers( "/api/v1/me").hasRole("USER")
+                        .requestMatchers( "/api/v1/users/**").hasRole("USER")
+                        .requestMatchers( "/api/v1/friends/**").hasRole("USER")
+
+                        // .requestMatchers( "/api/v1/test/admin").hasRole("ADMIN")  /api/v1/friends/request
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/**").permitAll()
+                        .requestMatchers("/error").permitAll()
+
+        );
+
+        http.addFilterBefore(
+                jwtAuthFilter,
+                UsernamePasswordAuthenticationFilter.class
+        );
+
+        // use HTTP Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable Cross Site Request Forgery (CSRF)
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+
+}
