2d42c36da3c208bf08a189a0639c0d193fd8a280
Yosef <yosef.saaid02@eng-st.cu.edu.eg>
2023-08-10 10:02:55 +0300

    adding Role & User Models

--- src/main/java/com/example/travelblog/models/Role.java
+++ src/main/java/com/example/travelblog/models/Role.java
@@ -1,9 +1,32 @@
 package com.example.travelblog.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 /*
  * @created 10/08/2023 - 8:52 AM
  * @project TravelBlog
  * @author Yosef Adel Mahmoud Saaid
  */
+@Entity
+@Table(name = "role")
+@Setter
+@Getter
+@NoArgsConstructor
+@ToString
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/com/example/travelblog/models/User.java
+++ src/main/java/com/example/travelblog/models/User.java
@@ -5,5 +5,78 @@ package com.example.travelblog.models;
  * @project TravelBlog
  * @author Yosef Adel Mahmoud Saaid
  */
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
+@Entity
+@Data
+@Table(name = "users")
+@ToString
 public class User {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "picture")
+    private String picture;
+
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Collection<Role> roles ;//new ArrayList<Role>(Arrays.asList(new Role("ROLE_USER")));
+
+    // TODO: 10/08/2023 add posts
+
+
+    @Column(name = "created_at", nullable = true, updatable = false)
+    private java.sql.Date createdAt;
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+
+    }
+
+    public User() {
+    }
+
+    public void addRole(Role role) {
+        if (roles == null) {
+            roles = new ArrayList<>();
+        }
+        roles.add(role);
+    }
+
+    public Collection<? extends GrantedAuthority> mapRolesToAuthorities() {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+
+    }
+
+    public boolean isAdmin() {
+        return roles.stream().anyMatch(role -> role.getName().equals("ROLE_ADMIN"));
+    }
+
+
+
+
 }
